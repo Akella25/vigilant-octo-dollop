b537b0e2978f2bb0c8cd11caa091fdfad63abb5a
Akella25 <xaxatun2517@gmail.com>
2021-09-24 23:40:28 +0300

    rrr

---
+++ lms/Class_stydent.py
@@ -0,0 +1,31 @@
+class Student:
+
+    student = []
+
+    def __init__(self, name,last_name,email, age, address, gender):
+        self.name = name
+        self.last_name = last_name
+        self.email = email
+        self.address = address
+        self.age = age
+        self.gender = gender
+        Student.student.append(self.name)
+
+
+class Group(Student):
+
+
+    def __init__(self,name):
+        self.name = name
+
+
+
+
+tud = Student('Tim','Kolins','ddd@mail', 22, 'sdfdfffff','man')
+ud = Student('Ti5555','Kolins','ddd@mail', 22, 'sdfdfffff','man')
+
+opg = Group('jj')
+print(opg.student)
+#print (tud.print_strudents_list)
+#print(tud.spiso)
+
